63a2cebda8eb7bb743a51222011ba47facfeeada
Rajamani Murugan <rajamanimurugan@Rajamanis-MacBook-Pro.local>
2020-02-21 00:31:52 -0800

    Initial COmmit

---
+++ src/main/java/com/demo/MyRetailRestService/MyRetailRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.MyRetailRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRetailRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRetailRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/MyRetailRestService/MyRetailRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.MyRetailRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRetailRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
